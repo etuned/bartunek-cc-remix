a0bccd9b79b54459558030d052a25ae8a64252fd
Edwin Bartunek <edwin@devhart.io>
2022-02-07 17:43:33 +0900

    content updates

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -64,20 +64,9 @@ export default function Index() {
         <h2>Edwin Bartunek</h2>
         <p>Educator and Software Developer</p>
         <p>
-          Feel free to take a look around the code to see how Remix does things,
-          it might be a bit different than what you’re used to. When you're
-          ready to dive deeper, we've got plenty of resources to get you
-          up-and-running quickly.
-        </p>
-        <p>
-          Check out all the demos in this starter, and then just delete the{" "}
-          <code>app/routes/demos</code> and <code>app/styles/demos</code>{" "}
-          folders when you're ready to turn this into your next project.
+          I am currently looking for new opportunities. Stay tuned for more updates in the coming weeks.
         </p>
       </main>
-      <aside>
-        <h2>More content coming.</h2>
-      </aside>
     </div>
   );
 }
